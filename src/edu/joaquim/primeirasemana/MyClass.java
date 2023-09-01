8ef628764bb443c5ba658440032a97ef72514966
jokaperes <jokaperes@gmail.com>
2023-09-01 16:38:31 -0400

    update

--- src/edu/joaquim/primeirasemana/MyClass.java
+++ src/edu/joaquim/primeirasemana/MyClass.java
@@ -1,15 +1,27 @@
 package edu.joaquim.primeirasemana;
 
+
 public class MyClass {
     public static void main(String[] args) {
         String primeiro = "Joaquim";
-        String segundo = "NetOO";
-        String nomeCompleto2 = nomeCompleto(primeiro, segundo);
+        String segundo = "Neto";
+        int numero = 1;
+        int numero2 = 20;
+        final double PI = 3.14;
 
+
+        String nomeCompleto2;
+        nomeCompleto2 = nomeCompleto(primeiro, segundo);
+        int resultado = somar(numero, numero2);
+        System.out.println(resultado);
         System.out.println(nomeCompleto2);
+        System.out.println(PI);
 
     }
     public static String nomeCompleto(String primeiro, String segundo){
-        return "Resultado do metodo " + primeiro.concat(" ").concat(segundo);
+        return  primeiro.concat(" ").concat(segundo);
+    }
+    public static int somar(int numero, int numero2){
+        return numero + numero2;
     }
 }
